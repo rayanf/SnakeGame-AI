89295bc392bbde12feed2805a91a29b8e1c106db
rayanf <rayanforsat@gmail.com>
2021-09-07 17:15:07 +0430

    before tails run

--- main.py
+++ main.py
@@ -2,17 +2,24 @@ import pygame
 import sys  
 import random
 
-class Sprite(pygame.sprite.Sprite):  
+
+
+class Player(pygame.sprite.Sprite):  
     def __init__(self, pos):  
         pygame.sprite.Sprite.__init__(self)  
         self.image = pygame.Surface([10, 10])  
         self.rect = self.image.get_rect()  
+        self.image.fill((155, 0, 155))  
+        self.move = [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN]  
+        self.vx = 10
+        self.vy = 10
         self.rect.center = pos  
         self.Direction = None
         self.headPos = pos
-        self.tails = []
         self.length = 0
-        
+        self.tails = []
+
+    
     def get_direction(self,pressed):
         for i in range(4):
             if pressed[self.move[i]]:
@@ -38,16 +45,51 @@ class Sprite(pygame.sprite.Sprite):
 
 
 
+class Food(pygame.sprite.Sprite):  
+    def __init__(self, pos):  
+        pygame.sprite.Sprite.__init__(self)  
+        self.image = pygame.Surface([10, 10])  
+        self.rect = self.image.get_rect()  
+        self.rect.center = pos  
+        self.image.fill((110, 215, 45))  
+
 def generate_food(food_group):
     x = random.randint(0,600)
     y = random.randint(0,600)
 
-    food = Sprite([x,y])
+    food = Food([x,y])
     food.image.fill((110, 215, 45))
  
     food_group.add(food)
 
     return [x,y]
+
+class Tail(pygame.sprite.Sprite):  
+    def __init__(self, pos):  
+        pygame.sprite.Sprite.__init__(self)  
+        self.image = pygame.Surface([10, 10])  
+        self.rect = self.image.get_rect()  
+        self.rect.center = pos  
+        self.image.fill((255, 100, 255)) 
+        self.Direction = None
+        self.move = [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN]  
+        self.vx = 10
+        self.vy = 10
+
+    def run(self):
+        if self.Direction != None:
+            for i in range(2):  
+                if self.Direction == self.move[i]:  
+                    self.rect.x += self.vx * [-1, 1][i]  
+
+            for i in range(2):  
+                if self.Direction == self.move[2:4][i]:  
+                    self.rect.y += self.vy * [-1, 1][i] 
+
+
+def move():
+    pass
+
 
 def main():  
     pygame.init()  
@@ -56,43 +98,51 @@ def main():
     bg = [100, 100, 100]  
     size =[600, 600]  
     screen = pygame.display.set_mode(size)  
-    player = Sprite([30, 30])  
 
-    player.move = [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN]  
-    player.vx = 10
-    player.vy = 10
-    player.image.fill((255, 100, 255))  
-
-    food = Sprite([100, 60])  
-    food.image.fill((110, 215, 45))  
-
-    food_group = pygame.sprite.Group()  
-    food_group.add(food)  
-  
+    player = Player([30, 30])  
     player_group = pygame.sprite.Group()  
     player_group.add(player)  
 
+    food = Food([100, 60])  
+    food_group = pygame.sprite.Group()  
+    food_group.add(food)  
+  
+    tail_group = pygame.sprite.Group()  
+
     while True:  
+        screen.fill(bg)  
         for event in pygame.event.get():  
             if event.type == pygame.QUIT:  
                 return False
 
         pressed = pygame.key.get_pressed()
-
         player.get_direction(pressed)
         player.run()
-          
-        screen.fill(bg)  
+
+        
       
         hit = pygame.sprite.spritecollide(player, food_group, True)  
-
         if hit: 
             generate_food(food_group) 
-            
+            player.length += 1
+            tail = Tail([player.rect.x,player.rect.y])
+            tail.Direction = player.Direction
+            tail_group.add(tail)
+            player.tails.append(tail)
 
+        # else:
+        #     try:
+        #         player.get_direction(pressed)
+        #         tail.Direction = player.Direction
+        #         player.run()
+        #         tail_group.run()
+        #     except:
+        #         player.get_direction(pressed)
+        #         player.run()
 
         player_group.draw(screen)  
-        food_group.draw(screen)  
+        food_group.draw(screen) 
+        tail_group.draw(screen) 
         pygame.display.update()  
         clock.tick(fps)  
     pygame.quit()  
