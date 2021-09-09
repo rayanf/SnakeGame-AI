4a91f6d4b6b529835513ce4760d53516be6f6455
rayanf <rayanforsat@gmail.com>
2021-09-10 00:18:11 +0430

    add plot

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 from game import snake_game
 import pygame
 from agent import Agent
+from plot import plot
 pygame.init()
 
 
@@ -43,7 +44,7 @@ def train():
             Tscore += score
             mean_score = Tscore / agent.n_games
             plot_mean_scores.append(mean_score)
-            # plot(plot_scores, plot_mean_scores)
+            plot(plot_scores, plot_mean_scores)
             game.reset()
 
 
