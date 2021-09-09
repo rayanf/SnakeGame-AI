9f240b990eebe28381c4a803ab1e3022130d8bb7
rayanf <rayanforsat@gmail.com>
2021-09-09 21:39:42 +0430

    change names

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ pygame.init()
 def train():
     plot_scores = []
     plot_mean_scores = []
-    total_score = 0
+    Tscore = 0
     record = 0
     agent = Agent()
     game = snake_game()
@@ -15,19 +15,19 @@ def train():
     while True:
         before_state = agent.get_state(game)
 
-        final_move, train_move = agent.get_action(before_state,game)
+        pygame_format_move, string_format_move = agent.get_action(before_state,game)
 
-        reward, GameOver, score = game.one_step(final_move)
+        reward, GameOver, score = game.one_step(pygame_format_move)
 
         after_state = agent.get_state(game)
 
-        train_moves = [0,0,0]
+        train_vector = [0,0,0]
         move_options = ['forward' ,'left' ,'right']
-        train_moves[move_options.index(train_move)] = 1
+        train_vector[move_options.index(string_format_move)] = 1
 
-        agent.train_short_memory(before_state, train_moves, reward, after_state, GameOver)
+        agent.train_short_memory(before_state, train_vector, reward, after_state, GameOver)
 
-        agent.save_memory(before_state, final_move, reward, after_state, GameOver)
+        agent.save_memory(before_state, pygame_format_move, reward, after_state, GameOver)
 
         if GameOver:
             agent.n_games += 1
@@ -37,11 +37,11 @@ def train():
                 record = score
                 agent.model.save()
 
-            print('Game', agent.n_games, 'Score', score, 'Record:', record)
+            print('Game: ', agent.n_games, 'Score:', score, 'Record:', record)
 
             plot_scores.append(score)
-            total_score += score
-            mean_score = total_score / agent.n_games
+            Tscore += score
+            mean_score = Tscore / agent.n_games
             plot_mean_scores.append(mean_score)
             # plot(plot_scores, plot_mean_scores)
             game.reset()
